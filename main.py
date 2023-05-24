036ea406054814851fbdd4f36f28263bf992c12d
Bentroen <bemcdc@gmail.com>
2023-05-24 02:48:31 -0300

    Adicionar código inicial rede neural
    Co-Authored-By: Nicolas Vycas Nery <vycasnicolas@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -31,7 +33,22 @@ outputs = [
 ]
 
 
-def read_tiles_from_image(image_path):
+class NeuralNetwork(nn.Module):
+    def __init__(self, nnets: int):
+        super(NeuralNetwork, self).__init__()
+        # first layer nnets neurons and tanh activation
+        self.fc1 = nn.Linear(20, nnets)
+        self.fc1.activation = nn.Tanh()
+        # second layer 10 neurons and linear activation
+        self.fc2 = nn.Linear(nnets, 10)
+        self.model = nn.Sequential(self.fc1, self.fc2)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+def read_tiles_from_image(image_path: str) -> List[List[int]]:
     # open the image "numeros.png"
     img = Image.open(image_path)
     # the image is a 40 x 5 image
@@ -61,14 +78,127 @@ def read_tiles_from_image(image_path):
     return numbers_list
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(10, 10)  # 10 inputs, 10 outputs
+def train_network(
+    input_tensors: torch.Tensor,
+    output_tensors: torch.Tensor,
+    nnets: int,
+    epochs: int,
+    goal: float,
+    learning_rate: float,
+    momentum: float,
+) -> NeuralNetwork:
+    # Mean Squared Loss
+    criterion = nn.MSELoss()
+    model = NeuralNetwork(nnets)
+    # stochastic gradient descent
+    optimizer = optim.SGD(  # Stochastic Gradient Descent/Gradient Descent with Momentum (TRAINGDM Equivalent)
+        model.parameters(), lr=learning_rate, momentum=momentum
+    )
 
-    def forward(self, x):
-        x = torch.sigmoid(self.fc1(x))
-        return x
+    # Train the neural network
+    for epoch in range(epochs):
+        # Forward pass
+        outputs = model(input_tensors)
+        loss = criterion(outputs, output_tensors)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 1000 == 0:
+            print(
+                f"Epoch [{epoch}/{epochs}], Loss: {loss.item():.6f}, Learning Rate: {learning_rate}, Momentum: {momentum}"
+            )
+
+        if loss.item() < goal:
+            break
+
+    print(f"Final loss: {loss.item():.6f}")
+    return model
+
+
+def make_flip_n_bits(bits: torch.Tensor, n: int = 1) -> torch.Tensor:
+    import random
+
+    randomIdxs = random.sample(range(0, len(bits)), n)
+    for i in randomIdxs:
+        bits[i] = 1 if bits[i] == 0 else 0
+    return bits
+
+
+def test_network(
+    model: NeuralNetwork, input_tensors: torch.Tensor, output_tensors: torch.Tensor
+):
+    original_input_tensors = input_tensors.clone()
+
+    # Test the neural network
+    print("Testing the neural network")
+    with torch.no_grad():
+        outputs = model(input_tensors)
+        print(f"Predicted: {outputs}")
+        print(f"Actual: {output_tensors}")
+
+    input_tensors = original_input_tensors.clone()
+
+    # flip 1 bit in each input and test again
+    print("Testing the neural network with 1 bit flipped in each input")
+    for i in range(len(input_tensors)):
+        input_tensors[i] = make_flip_n_bits(input_tensors[i], 1)
+    with torch.no_grad():
+        outputs = model(input_tensors)
+        print(f"Predicted: {outputs}")
+        print(f"Actual: {output_tensors}")
+
+    input_tensors = original_input_tensors.clone()
+
+    # flip 2 bits in each input and test again
+    print("Testing the neural network with 2 bits flipped in each input")
+    for i in range(len(input_tensors)):
+        input_tensors[i] = make_flip_n_bits(input_tensors[i], 2)
+    with torch.no_grad():
+        outputs = model(input_tensors)
+        print(f"Predicted: {outputs}")
+        print(f"Actual: {output_tensors}")
+
+    input_tensors = original_input_tensors.clone()
+
+    # flip 3 bits in each input and test again
+    print("Testing the neural network with 3 bits flipped in each input")
+    for i in range(len(input_tensors)):
+        input_tensors[i] = make_flip_n_bits(input_tensors[i], 3)
+    with torch.no_grad():
+        outputs = model(input_tensors)
+        print(f"Predicted: {outputs}")
+        print(f"Actual: {output_tensors}")
+
+
+def tests(input_tensors: torch.Tensor, output_tensors: torch.Tensor):
+    train_params = [
+        {"epochs": 10000, "goal": 0.0005, "learning_rate": 0.1, "momentum": 0.0},
+        {"epochs": 10000, "goal": 0.0005, "learning_rate": 0.4, "momentum": 0.0},
+        {"epochs": 10000, "goal": 0.0005, "learning_rate": 0.9, "momentum": 0.0},
+        {"epochs": 10000, "goal": 0.0005, "learning_rate": 0.1, "momentum": 0.4},
+        {"epochs": 10000, "goal": 0.0005, "learning_rate": 0.9, "momentum": 0.4},
+    ]
+    neural_networks = [15, 25, 35]
+    for nnets in neural_networks:
+        for params in train_params:
+            epochs = params["epochs"]
+            goal = params["goal"]
+            learning_rate = params["learning_rate"]
+            momentum = params["momentum"]
+
+            trained_model = train_network(
+                input_tensors,
+                output_tensors,
+                nnets,
+                epochs,
+                goal,
+                learning_rate,
+                momentum,
+            )
+            test_network(trained_model, input_tensors, output_tensors)
 
 
 def main():
@@ -78,32 +208,7 @@ def main():
     output_tensors = torch.tensor(
         [list(map(int, outp)) for outp in outputs], dtype=torch.float
     )
-
-    # Initialize the neural network
-    model = NeuralNetwork()
-
-    # Define the loss function and optimizer
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-
-    # Train the neural network
-    for epoch in range(1000):
-        optimizer.zero_grad()  # Zero the gradients
-        outputs = model(input_tensors)  # Forward pass
-        loss = criterion(outputs, output_tensors)  # Compute the loss
-        loss.backward()  # Backward pass
-        optimizer.step()  # Update the weights
-
-        if epoch % 100 == 0:
-            print(f"Epoch: {epoch+1}, Loss: {loss.item():.4f}")
-
-    # Test the model on a sample input
-    sample_input = torch.tensor(
-        [list(map(int, "01101001001001001111"))], dtype=torch.float
-    )
-    prediction = model(sample_input)
-    predicted_digit = prediction.argmax().item()
-    print(f"Predicted digit: {predicted_digit}")
+    tests(input_tensors, output_tensors)
 
 
 if __name__ == "__main__":
